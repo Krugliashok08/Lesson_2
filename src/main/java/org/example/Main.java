7d4a3547c928018ba31bec7011b3dc259ca1eb72
krugliashok08 <annakm803@gmail.com>
2024-02-12 16:36:24 +0300

    Lesson_2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main (String [] args) {
+        printThreeWords();
+        checkSumSign();
+        printCol();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("sum is positive");
+        } else {
+            System.out.println("sum is negative");
+        }
+    }
+
+    public static void printCol() {
+        int value = 50;
+        if (value <=0) {
+            System.out.println("red");
+        } if (value>0 && value<100) {
+            System.out.println("yellow");
+        } if (value>100) {
+            System.out.println("green");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 15;
+        int b = 35;
+        if  (a>=b) {
+            System.out.println("a>=b");
+        } if (a<b) {
+            System.out.println("a<b");
+        }
+    }
+
+}
